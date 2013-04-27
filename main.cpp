2756b6994cab7c6dfeab3416bedc1ea1e5836a7f
blacklaw <2577927146@qq.com>
2013-04-27 13:49:01 +0800

    add class CALC

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <IOSTREAM.H>
+class CALC{
+private:int num;
+public:int getNum(){
+		   return num;
+	   }
+	   void setNum(int lNum){
+			num=lNum;
+	   }
+};
 main(){
-	int a,b;
+	int a;
+	CALC *calc=new CALC();
 	cin>>a;
-	cout<<a;
+	calc->setNum(a);
+	cout<<calc->getNum()<<endl;
 }
